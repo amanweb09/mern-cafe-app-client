e4f43e5cbd6ee5d6731dcbbbeaf1d789fd8df3f8
Aman <amankhanna224466@gmail.com>
2021-12-28 11:48:04 +0530

    final commit

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,7 +7,7 @@ const Cart = () => {
     const { cart, setCart } = useContext(CartContext);
     const [cartItems, setCartItems] = useState([]);
 
-    const {navlink, setnavlink} = useContext(NavlinkContext);
+    const { navlink, setnavlink } = useContext(NavlinkContext);
 
     useEffect(() => {
         setnavlink('cart')
@@ -21,20 +21,33 @@ const Cart = () => {
                 .then((res) => {
                     const items = res.data.items;
                     setCartItems(items);
+
                 })
                 .catch((err) => {
                     console.log(err);
                 })
         }
     }, [cart])
-    // const deleteItem = (pid) => {
-    //     let _cart = { ...cart };
-    //     delete _cart.items[pid]
 
-    //     setCart(_cart);
-    // }
+    const incItem = (_id) => {
+        let _cart = { ...cart }
+        let itemQty = _cart.items[_id];
 
+        itemQty += 1;
+        _cart.totalItems += 1
 
+        setCart(_cart)
+    }
+
+    const decItem = (_id) => {
+        let _cart = { ...cart }
+        let itemQty = _cart.items[_id];
+
+        itemQty -= 1;
+        _cart.totalItems -= 1
+
+        setCart(_cart)
+    }
 
     return (
         <div className='container mx-auto'>
@@ -50,9 +63,9 @@ const Cart = () => {
                                         <img className='w-32 h-32' src={item.image} alt="" />
                                         <h1 className="font-semibold text-xl">{item.name}</h1>
                                         <div className='flex items-center justify-center'>
-                                            <button className="w-10 h-10 bg-red-500 rounded-md mx-4">-</button>
-                                            <span className='font-bold'>1</span>
-                                            <button className="w-10 h-10 bg-red-500 rounded-md mx-4">+</button>
+                                            <button onClick={() => { decItem(item._id) }} className="w-10 h-10 bg-red-500 rounded-md mx-4">-</button>
+                                            <span className='font-bold'>{cart.items[item._id]}</span>
+                                            <button onClick={() => { incItem(item._id) }} className="w-10 h-10 bg-red-500 rounded-md mx-4">+</button>
                                         </div>
                                         <h2 className='font-semibold text-xl text-red-500'>Rs. {item.price}</h2>
                                         <button className='bg-black w-40 h-12 text-white rounded-full font-bold'>Delete</button>
